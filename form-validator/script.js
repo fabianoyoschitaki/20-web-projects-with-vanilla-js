e52254a68d9258d8a99ded44426f5c9e280018a9
Fabiano Yoschitaki <fabiano.yoschitaki@bairesdev.com>
2020-06-29 03:08:40 -0300

    form validator end

--- form-validator/script.js
+++ form-validator/script.js
@@ -6,7 +6,6 @@ const email = document.getElementById('email');
 const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
 
-
 // show input error message
 function showError(input, message){
     // gets parent form control and adds error class. this affects also <small> 
@@ -18,9 +17,13 @@ function showError(input, message){
 }
 
 // check valid email
-function isValidEmail(email) {
+function checkEmail(input) {
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
+    if (re.test(String(input.value.trim()).toLowerCase())) {
+        showSuccess(input);
+    } else {
+        showError(input, 'Email is not valid');
+    }
 }
 
 // show success outline
@@ -30,11 +33,54 @@ function showSuccess(input){
     formControl.className = 'form-control success';
 }
 
+// check required form fields
+function checkRequired(inputArray) {
+    inputArray.forEach(input => {
+        if (input.value.trim() === ''){
+            // use ES6 template string instead of concatenating
+            showError(input, `${getFieldName(input)} is required`);
+        } else {
+            showSuccess(input);
+        }
+    });
+}
+
+// check input length
+function checkLength(input, min, max){
+    if (input.value.length < min){
+        showError(input, `${getFieldName(input)} must be at least ${min} characters`);
+    } else if (input.value.length > max){
+        showError(input, `${getFieldName(input)} must be less than ${max} characters`);
+    } else {
+        showSuccess(input);
+    }
+}
+
+// check passwords match
+function checkPasswordsMatch(input1, input2){
+    if (input1.value !== input2.value){
+        showError(input2, 'Passwords do not match');
+    }
+}
+
+// get fieldname uppercasing first letter
+function getFieldName(input){
+    // TODO slice vs substring?
+    return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+
 // submit form event listener
 form.addEventListener('submit', function(e){
     // prevents form from submitting
     e.preventDefault();
-    
+    // check required fields
+    checkRequired([username, email, password, password2]);
+    // check field lengths (min max)
+    checkLength(username, 3, 15);
+    checkLength(password, 6, 25);
+    checkEmail(email);
+    checkPasswordsMatch(password, password2);
+    /** ugly
     if (username.value === ''){
         // show error
         showError(username, 'Username is required');
@@ -64,5 +110,5 @@ form.addEventListener('submit', function(e){
         showError(password2, 'Password 2 is required');
     } else {
         showSuccess(password2);
-    }
+    } */
 });
